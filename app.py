e6e572d7468af7e412bf257db19128c1ce75cf1a
Aayesha-lkw <aayesha@walter-group.com>
2025-10-01 06:37:23 +0000

    updated work repo

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=8080, log_level="info")
